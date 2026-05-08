7fd938eac6ad425af91383885fca1e642380a6ae
rsderamos <rsderamos01@gmail.com>
2026-05-08 17:40:17 +0800

    Auto update data

--- data/scenarios.js
+++ data/scenarios.js
@@ -1,4 +1,4 @@
 var scenario =
 [
-{"name": "prep_dry_sim1", "long_name": "Philippines Forecast (prep_dry_sim1)", "description": "This is the Day 0 to Day 3 forecast for the Philippines.\nInput: ECMWF", "lon": 125.0, "lat": 11.24, "zoom": 8, "cycle": "2026-05-07T18:00:00", "cycle_string": "20260507_18z", "cycle_mode": "continuous", "meteo_string": "20260508_00z (ecmwf_meteo_data)", "duration": "102.0", "last_update": "2026/05/08 09:23:01 (UTC)", "previous_cycles": ["20260508_06z", "20260508_00z", "20260507_18z", "20260507_12z", "20260507_06z", "20260507_00z", "20260506_18z"]}
+{"name": "prep_dry_sim1", "long_name": "Philippines Forecast (prep_dry_sim1)", "description": "This is the Day 0 to Day 3 forecast for the Philippines.\nInput: ECMWF", "lon": 125.0, "lat": 11.24, "zoom": 8, "cycle": "2026-05-08T00:00:00", "cycle_string": "20260508_00z", "cycle_mode": "continuous", "meteo_string": "20260508_00z (ecmwf_meteo_data)", "duration": "102.0", "last_update": "2026/05/08 09:40:07 (UTC)", "previous_cycles": ["20260508_06z", "20260508_00z", "20260507_18z", "20260507_12z", "20260507_06z", "20260507_00z", "20260506_18z"]}
 ]
