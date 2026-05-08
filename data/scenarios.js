8484279c5f6ea7bc296ab929650861ab4b911dde
rsderamos <rsderamos01@gmail.com>
2026-05-08 10:43:31 +0800

    upload 1st data

---
+++ data/scenarios.js
@@ -0,0 +1,4 @@
+var scenario =
+[
+{"name": "prep_dry_sim1", "long_name": "Philippines Forecast (prep_dry_sim1)", "description": "This is the Day 0 to Day 3 forecast for the Philippines.\nInput: ECMWF", "lon": 125.0, "lat": 11.24, "zoom": 8, "cycle": "2026-05-07T18:00:00", "cycle_string": "20260507_18z", "cycle_mode": "continuous", "meteo_string": "20260507_18z (ecmwf_meteo_data)", "duration": "102.0", "last_update": "2026/05/08 02:00:56 (UTC)", "previous_cycles": ["20260507_18z", "20260507_12z", "20260507_06z", "20260507_00z", "20260506_18z"]}
+]
